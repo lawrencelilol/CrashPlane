c34c60987f60191a2a80ac73c571473526e97839
lawrence.li <tiantonl@andrew.cmu.edu>
2022-11-14 20:22:13 -0500

    first commit

---
+++ CrashPlaneUITests/CrashPlaneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CrashPlaneUITestsLaunchTests.swift
+//  CrashPlaneUITests
+//
+//  Created by Lawrence on 11/13/22.
+//
+
+import XCTest
+
+final class CrashPlaneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
